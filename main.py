2687170b19168fe626c252a511308c30b010a279
tony430 <tonychang430@hotmail.com>
2021-05-20 17:29:00 +0800

    Added a delay in each search of classes

--- main.py
+++ main.py
@@ -8,30 +8,29 @@ if __name__ == '__main__':
     current_semester = "FA21"
     notification = Notify()
     notification.application_name = "UIUC Registration Bot"
-    notification.title = "CLASS OPEN"
     notification.icon = "illinoisIcon.png"  # icons might not work on mac
     notification.audio = "Sound2.wav"
 
-    #double and triple check that the CRN is correct
-
-    courses_you_want  = [("CS 233",63735)]
-
-    for course in courses_you_want:
-        section = ua.get_course(course[0] + " " +  current_semester).sections
-        cour = ua.get_course(course[0] + " " +  current_semester)
-        for sec in section:
-            print(sec.crn == course[1])
-            if sec.crn == course[1] and 'open' in sec.registration_status.lower():
-                notification.message =  cour.name + \
-                                       " with this CRN" + str(sec.crn)  + "is now OPEN!!"
-                notification.send(block=False)
-
-
-
+    section = ua.get_course("CS 361 FA21").sections
     for sec in section:
         print("CS 233" + " " + sec.section_number + " " + str(sec.crn) + "  " + sec.registration_status)
 
+    # double and triple check that the CRN is correct
+    # enter the the courses in this format ("SUBJECT NUMBER", CRN)
+    # EX: courses_you_want  = [("CS 233",63735), ("CS 233", 63733), ("CS 361", 66305)]
+    courses_you_want = [("CS 233", 63735), ("CS 233", 63733), ("CS 361", 66305)]
+    while (True):
+        for course in courses_you_want:
+            section = ua.get_course(course[0] + " " + current_semester).sections
+            cour = ua.get_course(course[0] + " " + current_semester)
+            for sec in section:
+                if sec.crn == course[1] and 'open' in sec.registration_status.lower():
+                    notification.title = cour.name + " OPEN"
+                    notification.message = "The class" + " with this CRN" + str(sec.crn) + " is now OPEN!!"
+                    notification.send(block=False)
 
-        #print()
-    #print_hi('PyCharm')
-
+        # The time interval that the code searches for the class in secconds
+        # 4 hours = 60 * 60 * 4
+        # 1 minute = 60
+        # 10 sec = 10
+        time.sleep(60)
