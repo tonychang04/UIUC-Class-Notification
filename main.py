289b1096d0d54ca4f3ac88302e31a4ede198172d
tony430 <tonychang430@hotmail.com>
2021-05-21 13:58:02 +0800

    Installed libnotify

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
                     notification.message = "The class" + " with this CRN" + str(sec.crn) + " is now OPEN!!"
                     notification.send(block=False)
 
+
         # Enter the time interval that the code searches for the class in second
         # 4 hours = 60 * 60 * 4
         # 5 minute = 60 * 5
